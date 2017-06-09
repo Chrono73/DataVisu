60f0df727536cb0febae6ed47ca4f99bbbc9946e
Martin L <martin.chronophage@orange.fr>
2017-06-09 15:48:42 -0400

    Updated walker.py

--- walker.py
+++ walker.py
@@ -14,6 +14,11 @@ class Walker():
         Constructor
         '''
         self.graph = graph
+
+        self.levelZeroPtr = None
+        self.xTopAdjustment = 0
+        self.yTopAdjustment = 0
+
         self.LevelSeparation = 2
         self.MaxDepth = math.inf
         self.SiblingSeparation = 1
@@ -62,3 +67,37 @@ class Walker():
             break
         except StopIteration:
             return None
+
+    def xCoord(node):
+        return self.graph['viewLayout'][node].getX()
+
+    def yCoord(node):
+        return self.graph['viewLayout'][node].getY()
+
+    def prelim(node):
+        return self.graph['prelim'][node]
+
+    def modifier(node):
+        return self.graph['modifier'][node]
+
+    '''
+    def leftNeighbor(node):
+        return
+    '''
+
+    def positionTree(node):
+        if (node == None):
+            return True
+        else:
+            '''
+            initPrevNodeList()
+            firstWalk(node, 0)
+
+            xTopAdjustment = xCoord(node) - prelim(node)
+            yTopAdjustment = yCoord(node)
+
+            return secondWalk(node, 0, 0)
+            '''
+
+def main(graph):
+    print 'Compilation successful.'

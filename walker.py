3b0d38a4b61fecd85710b5168de0c598e0c25fba
Martin L <martin.chronophage@orange.fr>
2017-06-23 03:09:31 -0400

    MaJ de walker.py
    Fin de l'implémentation des fonctions auxilliaires, début de l'implémentation de l'algorithme même.

--- walker.py
+++ walker.py
@@ -7,43 +7,79 @@ Created on june 9th 2011
 from tulip import *
 import math
 
-class Walker():
+class Walker(object):
 
     def __init__(self, graph):
         '''
         Constructor
         '''
         self.graph = graph
+        n = self.graph.getNodes()
+        self.root = n.next()
+        
+        self.graph.getIntegerProperty('levelIndex')
 
         self.levelZeroPtr = list()
         self.xTopAdjustment = 0
         self.yTopAdjustment = 0
 
         self.LevelSeparation = 2
-        self.MaxDepth = math.inf
+        self.MaxDepth = self.graph['viewMetric'].getNodeMax()
         self.SiblingSeparation = 1
         self.SubtreeSeparation = 5
 
-    def parent(node):
-        try:
-            return self.graph.getInNodes(node).next()
-        except StopIteration:
-            return None
+        # Assigne a chaque node l'index de sa position sur sa ligne.
+    def initNodeLevelIds(self):
+        levelCounter = list()
+        for i in range(0, int(self.graph['viewMetric'][self.root])+1):
+            levelCounter.append(0)
+        nodes = self.graph.getNodes()
+        for n in nodes:
+            self.graph['levelIndex'][n] = levelCounter[self.level(n)]
+            levelCounter[self.level(n)] += 1
+    
+        # Renvoie la index-ieme node du niveau lvl du graphe
+    def getNthNodeAtLevel(self, lvl, index):
+        nodes = self.graph.getNodes()
+        for n in nodes:
+            if ((self.level(n) == lvl) and (self.graph['levelIndex'][n] == index)):
+                return n
+        return None
 
-    def hasChild(node):
+        # Renvoie le parent de la node actuelle.
+    def parent(self, node):
+        try:
+            res = self.graph.getInNodes(node).next()
+        except:
+            res = None
+        return res
+
+        # Renvoie le niveau de la node actuelle
+    def level(self, node):
+        level = 0
+        current = node
+        while not (self.parent(current)==None):
+            current = self.parent(current)
+            level += 1
+        return level
+
+        # Verifie si la node possede un enfant (si c'est une feuille)
+    def hasChild(self, node):
         try:
             self.graph.getOutNodes(node).next()
             return True
         except StopIteration:
             return False
 
-    def firstChild(node):
+        # Renvoie le premier enfant de la node
+    def firstChild(self, node):
         try:
             return self.graph.getOutNodes(node).next()
         except StopIteration:
             return None
 
-    def hasLeftSibling(node):
+        # Verifie si la node possede un voisin de gauche
+    def hasLeftSibling(self, node):
         parent = self.parent(node)
         siblingList = self.graph.getOutNodes(parent)
         try:
@@ -59,7 +95,8 @@ class Walker():
         except StopIteration:
             return False
 
-    def leftSibling(node):
+        # Renvoie le voisin de gauche de la node
+    def leftSibling(self, node):
         parent = self.parent(node)
         siblingList = self.graph.getOutNodes(parent)
         try:
@@ -75,7 +112,8 @@ class Walker():
         except StopIteration:
             return None
 
-    def hasRightSibling(node):
+        # Verifie si la node possede un voisin de droite
+    def hasRightSibling(self, node):
         parent = self.parent(node)
         siblingList = self.graph.getOutNodes(parent)
         try:
@@ -90,7 +128,8 @@ class Walker():
         except StopIteration:
             return False
 
-    def rightSibling(node):
+        # Renvoie le voisin de droite de la node
+    def rightSibling(self, node):
         parent = self.parent(node)
         siblingList = self.graph.getOutNodes(parent)
         try:
@@ -101,32 +140,29 @@ class Walker():
         except StopIteration:
             return None
 
-    def xCoord(node):
+        # Renvoie la coordonnee x de la node
+    def xCoord(self, node):
         return self.graph['viewLayout'][node].getX()
 
-    def yCoord(node):
+        # Renvoie la coordonnee y de la node
+    def yCoord(self, node):
         return self.graph['viewLayout'][node].getY()
 
-    '''
-    def prelim(node):
-        return self.graph['prelim'][node]
-
-    def modifier(node):
-        return self.graph['modifier'][node]
-    '''
-    def prelim(node, val):
+        # Permet de definir la coordonnee x preliminaire de la node
+    def prelim(self, node, val):
         self.graph['prelim'][node] = val
 
-    def modifier(node, val):
+        # Permet de definir le modificateur de la node
+    def modifier(self, node, val):
         self.graph['modifier'][node] = val
 
-    '''
-    def leftNeighbor(node):
-        return
-    '''
+        # Renvoie la node devant etre disposee a gauche de la node en parametre
+    def leftNeighbor(self, node):
+        return self.getNthNodeAtLevel(self.level(node), self.graph['levelIndex'][node]-1)
+            
 
-    def initPrevNodeList():
-        for i in range(0, self.graph['viewMetric'].getNodeMax()):
+    def initPrevNodeList(self):
+        for i in range(0, self.graph['viewMetric'][root]):
             self.levelZeroPtr.append(None)
 
     '''
@@ -135,7 +171,7 @@ class Walker():
         CheckExtentsRange is useless in the context of Tulip
     '''
 
-    def getLeftMost(node, level, depth):
+    def getLeftMost(self, node, level, depth):
         if (level >= depth):
             return node
         elif (self.graph['viewMetric'][node]==0):
@@ -148,7 +184,7 @@ class Walker():
                 leftmost = self.getLeftMost(rightmost, level+1, depth)
             return leftmost
 
-    def positionTree(node):
+    def positionTree(self, node):
         if (node == None):
             return True
         else:
@@ -164,3 +200,5 @@ class Walker():
 
 def main(graph):
     print 'Compilation successful.'
+    walk = Walker(graph)
+    walk.initNodeLevelIds()
